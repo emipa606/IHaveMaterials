59a6ca340b62c3c1463cec03acee1ee47613317a
Mlie <emipa606@users.noreply.github.com>
2023-11-21 21:03:14 +0100

    First publish. Used DeepL to update translations for ChineseSimplified, French, German

---
+++ README.md
@@ -0,0 +1,32 @@
+# IHaveMaterials
+
+![Image](https://i.imgur.com/buuPQel.png)
+
+Update of Ben Lubars mod https://steamcommunity.com/sharedfiles/filedetails/?id=2613994109
+
+![Image](https://i.imgur.com/pufA0kM.png)
+
+	
+![Image](https://i.imgur.com/Z4GOv8H.png)
+
+You can always place blueprints, even if you have no materials.
+
+The materials are configurable, but by default it forces wood, steel, cloth, and any stone type found naturally in the current map to act as if you have some.
+
+Compatibility:
+Does not make any changes to your save file. Safe to add or remove at any time.
+Conflicts with other mods that replace the Designator_Build.ProcessInput function. (I am not currently aware of any.)
+
+![Image](https://i.imgur.com/PwoNOj4.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib or the standalone https://steamcommunity.com/sharedfiles/filedetails/?id=2873415404]Uploader and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+-  Use https://github.com/RimSort/RimSort/releases/latest]RimSort to sort your mods
+
+
---
+++ Source/IHaveMaterials/Designator_Build_ProcessInput_Patch.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace IHaveMaterials;
+
+[HarmonyPatch]
+internal static class Designator_Build_ProcessInput_Patch
+{
+    private static readonly MethodInfo CheckCanInteract =
+        AccessTools.Method(typeof(Designator_Build), "CheckCanInteract");
+
+    private static readonly FieldInfo stuffDef = AccessTools.Field(typeof(Designator_Build), "stuffDef");
+    private static readonly FieldInfo writeStuff = AccessTools.Field(typeof(Designator_Build), "writeStuff");
+
+    [HarmonyPatch(typeof(Designator_Build), nameof(Designator_Build.ProcessInput), typeof(Event))]
+    [HarmonyPrefix]
+    private static bool Prefix(Designator_Build __instance, BuildableDef ___entDef)
+    {
+        if (!(bool)CheckCanInteract.Invoke(__instance, Array.Empty<object>()))
+        {
+            return true;
+        }
+
+        if (___entDef is not ThingDef { MadeFromStuff: true } thingDef)
+        {
+            return true;
+        }
+
+        var vanilla = __instance.Map.resourceCounter.AllCountedAmounts.Keys.Where(d =>
+            d.IsStuff && d.stuffProps.CanMake(thingDef) &&
+            (DebugSettings.godMode || __instance.Map.listerThings.ThingsOfDef(d).Count > 0));
+
+        // Non-vanilla stuff begins
+        var forced = IHaveMaterials.Instance.Stuff.Where(d => d.Value && d.Key.stuffProps.CanMake(thingDef))
+            .Select(d => d.Key);
+        if (IHaveMaterials.Instance.StonyFromMap)
+        {
+            var rockWalls = Find.World.NaturalRockTypesIn(__instance.Map.Tile);
+            var rockChunks = rockWalls.Select(t => t.building.mineableThing);
+            var rockBlocks = rockChunks.SelectMany(d => d.butcherProducts.Select(p => p.thingDef));
+            forced = forced.Union(rockChunks.Concat(rockBlocks)
+                .Where(d => d.IsStuff && d.stuffProps.CanMake(thingDef)));
+        }
+
+        var merged = vanilla.Union(forced).OrderByDescending(d => d.stuffProps.commonality)
+            .ThenBy(d => d.BaseMarketValue);
+        // Non-vanilla stuff ends
+
+        var options = merged.Select(item =>
+        {
+            string label;
+
+            if (__instance.sourcePrecept == null)
+            {
+                label = GenLabel.ThingLabel(___entDef, item);
+            }
+            else
+            {
+                label = "ThingMadeOfStuffLabel".Translate(item.LabelAsStuff, __instance.sourcePrecept.Label);
+            }
+
+            return new FloatMenuOption(label.CapitalizeFirst(), delegate
+            {
+                //BaseProcessInput();
+                Find.DesignatorManager.Select(__instance);
+                stuffDef.SetValue(__instance, item);
+                writeStuff.SetValue(__instance, true);
+            }, item)
+            {
+                tutorTag = $"SelectStuff-{thingDef.defName}-{item.defName}"
+            };
+        }).ToList();
+
+        if (options.Count == 0)
+        {
+            return true;
+        }
+
+        var floatMenu = new FloatMenu(options)
+        {
+            vanishIfMouseDistant = true,
+            onCloseCallback = delegate { writeStuff.SetValue(__instance, true); }
+        };
+        Find.WindowStack.Add(floatMenu);
+        Find.DesignatorManager.Select(__instance);
+
+        return false;
+    }
+
+    //[HarmonyReversePatch]
+    //[HarmonyPatch(typeof(Designator), nameof(Designator.ProcessInput), typeof(Event))]
+    //private static void BaseProcessInput()
+    //{
+    //    throw new NotImplementedException("reverse patch somehow failed");
+    //}
+}
